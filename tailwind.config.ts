1cfa2339f4da36c7d05e0e2538554957ae1e126d
JiinSeok <126224323+jiinseok@users.noreply.github.com>
2025-01-03 19:26:02 +0900

    feat: add font 'pretendard'

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  content: ['./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
         sans: [
-          "Inter",
-          "ui-sans-serif",
-          "system-ui",
-          "sans-serif",
-          "Apple Color Emoji",
-          "Segoe UI Emoji",
-          "Segoe UI Symbol",
-          "Noto Color Emoji",
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Apple SD Gothic Neo',
+          'Pretendard Variable',
+          'Pretendard',
+          'Roboto',
+          'Noto Sans KR',
+          'Segoe UI',
+          'Malgun Gothic',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'ui-sans-serif',
+          'system-ui',
+          'sans-serif',
         ],
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
